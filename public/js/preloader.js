998293f4dc691c090c0896b281843e415ce7055b
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-05-15 16:58:46 -0500

    force div mask styling

--- public/js/preloader.js
+++ public/js/preloader.js
@@ -1,13 +1,9 @@
 const body = document.querySelector('body');
 const loadingMask = document.querySelector('#loading-mask');
 
-loadingMask.style.display = 'block';
 body.style.overflow = 'hidden';
 
 document.addEventListener("DOMContentLoaded", () => {
-    window.setTimeout(() => {
-      console.log(document.readyState);
-      loadingMask.style.display = 'none';
-      body.style.overflow = 'initial';
-    }, 300);
+    loadingMask.style.display = 'none';
+    body.style.overflow = 'initial';
 });
