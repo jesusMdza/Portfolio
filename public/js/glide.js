f0686b425adc6cb45b12932c45f9b0cc42f47164
Jesus Mendoza <jesuszmendoza14@gmail.com>
2020-05-18 10:47:59 -0500

    add func to simulate click on slider

--- public/js/glide.js
+++ public/js/glide.js
@@ -1,5 +1,5 @@
 const glide1 = document.querySelector('.glide1');
-const glideSlides = document.querySelector('.glide__slides');
+const firstGlideBullet = document.querySelector('.glide__bullets').firstElementChild;
 
 if (glide1) {
   new Glide(glide1, {
@@ -11,11 +11,9 @@ if (glide1) {
   }).mount();
 }
 
-// fixes issue with current slide not being centered upon viewing
-function resetGliderWidth() {
-  glideSlides.setAttribute('style', 'transition: transform 0ms cubic-bezier(0.165, 0.84, 0.44, 1) 0s; width: 9444px; transform: translate3d(-1560px, 0px, 0px);');
-}
-
-if (glideSlides) {
-  resetGliderWidth();
-}
+// clicks slider button to center image
+document.addEventListener('DOMContentLoaded', () => {
+  if (firstGlideBullet) {
+    firstGlideBullet.click();
+  }
+});
